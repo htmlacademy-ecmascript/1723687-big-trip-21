64c558562d852d0bac715ec114deeee1ffcf3041
@Alina837-a <alishakuritsyna@yandex.ru>
2023-08-22 22:36:52 +0300

    добавляет моковые данные

---
+++ src/mock/destination.js
@@ -0,0 +1,19 @@
+import { getRandomArrayElement } from "../utils.js";
+import { CITIES, DESCROPTIONS } from "../const.js";
+
+function generateMockDestination(destinationId) {
+  const city = getRandomArrayElement(CITIES);
+  const description = getRandomArrayElement(DESCROPTIONS);
+  return {
+    id: crypto.randomUUID(),
+    description: description,
+    name: city,
+    pictures: {
+      src: `https://loremflickr.com/248/152?random=${crypto.randomUUID()}`,
+      description: `${city} description`,
+    },
+  };
+}
+
+export { generateMockDestination };
+
---
+++ src/mock/utils.js
@@ -0,0 +1,23 @@
+import dayjs from 'dayjs';
+import { getRandomPositiveInteger } from '../utils';
+import { Duration } from '../const';
+
+let date = dayjs().subtract(getRandomPositiveInteger(0, Duration.DAY), 'day').toDate()
+
+function getDate({next}) {
+    const minsGap = getRandomPositiveInteger(0, Duration.MIN);
+    const hoursGap = getRandomPositiveInteger(0, Duration.HOUR);
+    const daysGap = getRandomPositiveInteger(0, Duration.DAY);
+
+    if(next) {
+        date = dayjs(date) 
+            .add(minsGap, 'minute')
+            .add(hoursGap, 'hour')
+            .add(daysGap, 'day')
+            .toDate();
+    }
+
+    return date
+}
+
+export {getDate};
